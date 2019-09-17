1208f72faf0ee03df3fbb5e021ed221cc7b3b7ac
gnoale <guillaume.noale@botify.com>
2019-09-17 10:11:36 +0200

    add timer, multithreading is faster than spwawning multiple process in this case

--- influclient.py
+++ influclient.py
@@ -2,28 +2,41 @@
 
 # -*- coding: utf-8 -*-
 from influxdb import InfluxDBClient
-
 from multiprocessing import Pool
-
-# testing purpose
 import socket, random, os
-
-def main(host='localhost'):
- 
-    client = InfluxDBClient(host, use_udp=True, udp_port=8089)
-
-    #line_body = ["moult_points,host={},region=laborde simple_point={}".format(socket.gethostname(), random.randint(0,2000))]
+import functools
+import time
+from threading import Thread
 
 
-    line_body = ["moult_points,host={},region=laborde swf_call=1".format(socket.gethostname())]
+def timer(func):
+    """Print the runtime of the decorated function"""
+    @functools.wraps(func)
+    def wrapper_timer(*args, **kwargs):
+        start_time = time.clock()    # 1
+        value = func(*args, **kwargs)
+        end_time = time.clock()      # 2
+        run_time = end_time - start_time    # 3
+        print("Finished {0!r} in {1:.4f} secs".format(func.__name__, run_time ))
+        return value
+    return wrapper_timer
 
-    print(line_body)
+
+client = InfluxDBClient('localhost', use_udp=True, udp_port=8089)
+#line_body = ["moult_points,host={},region=laborde simple_point={}".format(socket.gethostname(), random.randint(0,2000))]
+line_body = ["moult_points,host={},region=laborde swf_call=1".format(socket.gethostname())]
 
 
-    pool = Pool(processes=1)
-
+@timer
+def push_data(client=client, data=line_body):
+    #pool = Pool(1)
     for i in range(1000):
-        pool.apply_async(client.send_packet(line_body, protocol=u'line'))
+        #pool.apply_async(client.send_packet(data, protocol=u'line'))
+        Thread(target=client.send_packet(data, protocol=u'line'))
+
+    #pool.close() 
+
 
 if __name__ == '__main__':
-    main()
+    while True:
+        push_data()
